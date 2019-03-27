4f72305dda7d304ab263491747936d2eebd14479
MrFuentes <seangit99@gmail.com>
2019-03-27 12:22:33 +0000

    made myshell.py pep8 compliant

--- myshell.py
+++ myshell.py
@@ -78,67 +78,83 @@ class SeaTurtle(Cmd):
                                 except IndexError:
                                     # If no filename specified
                                     print('Error: No filename given')
-                                    string = 'Usage: dir < {} >> <filename>'.format(data[0])
+                                    string = 'Usage: dir < {} >> <filename>'
+                                    string = string.format(data[0])
                                     print(string)
 
                             elif args[2] == '>':
                                 try:
                                     # overwrite the data in the file
-                                    overwrite(ls_dir(data[0]), args[3:])  
+                                    overwrite(ls_dir(data[0]), args[3:])
                                 except IndexError:
-                                    # Shows this error message if no filename specified
+                                    # If no filename specified
                                     print('Error: No filename given')
-                                    print('Usage: dir < {} >> <filename>'.format(data[0]))
-                            else:  
-                                # Prints the contents if standard output not being used
+                                    string = 'Usage: dir < {} >> <filename>'
+                                    string = string.format(data[0])
+                                    print(string)
+                            else:
+                                # If standard output not being used
+                                # Prints content
                                 print(ls_dir(data[0]))
                         except IndexError:
-                        print(ls_dir(data[0]))  # Prints the contents if standard output not being used
+                            # If standard output not being used
+                            # Prints content
+                            print(ls_dir(data[0]))
                     except IndexError:
-                            print('Error: No filename given')
-                            print('Usage: dir < <filename>')  # shows this error message if no filename was specified
-                elif args[1] == '>>':  # If using append output with a specific directory
-                    try:
-                        append(ls_dir(args[0]),args[2:])  # Appends the content from the named directory to the filename specified
-                    except IndexError:
+                        # shows this error message if no filename was specified
                         print('Error: No filename given')
-                        print('Usage: dir {} >> <filename>'.format(args[0])) # Shows this error if no filename is specified 
-                elif args[1] == '>':  # If using overwrite output with a specific directory
+                        print('Usage: dir < <filename>')
+                elif args[1] == '>>':
+                    # If using append output with a specific directory
                     try:
-                        overwrite(ls_dir(args[0]),args[2:])  # Overwrites the file's content with the contents of the listed directory
+                        # Appends to the filename specified
+                        append(ls_dir(args[0]), args[2:])
                     except IndexError:
+                        # Shows this error if no filename is specified
                         print('Error: No filename given')
-                        print('Usage: dir {} > <filename>'.format(args[0]))  # Shows this error if no filename is specified
+                        print('Usage: dir {} >> <filename>'.format(args[0]))
+                elif args[1] == '>':
+                    # If using overwrite output with a specific directory
+                    try:
+                        # Overwrites the contents of the listed directory
+                        overwrite(ls_dir(args[0]), args[2:])
+                    except IndexError:
+                        # Shows this error if no filename is specified
+                        print('Error: No filename given')
+                        print('Usage: dir {} > <filename>'.format(args[0]))
             except IndexError:
                 try:
-                    if args[0] == '>>':  # If using append output without a specified directory
+                    # If using append output without a specified directory
+                    if args[0] == '>>':
                         try:
-                            append(ls_dir(),args[1:])  # Append the content of the current directory to the specified file
+                            # Append to the specified file
+                            append(ls_dir(), args[1:])
                         except IndexError:
+                            # If no filename is specified
                             print('Error: No filename given')
-                            print('Usage: dir >> <filename>')  # Shows this error message if no filename is specified
-                    elif args[0] == '>':  # If using overwrite output without a specified directory
+                            print('Usage: dir >> <filename>')
+                    elif args[0] == '>':
+                        # If using overwrite without a specified directory
                         try:
-                            overwrite(ls_dir(),args[1:])  # Overwrites file's contents with the content of the current directory
+                            # Overwrites file's contents
+                            overwrite(ls_dir(), args[1:])
                         except IndexError:
+                            # If no filename is specified
                             print('Error: No filename given')
-                            print('Usage: dir > <filename>')  # Shows this error message if no filename is specified
+                            print('Usage: dir > <filename>')
                     else:
-                        print(ls_dir(args[0]))  # prints the content of the specified directory
+                        # prints the content of the specified directory
+                        print(ls_dir(args[0]))
                 except IndexError:
-                    print(ls_dir())  # Prints the content of the current directory if no directory is specified
-
-        
+                    # Prints the content of the current directory
+                    print(ls_dir())
 
         def do_clr(self, arg):
-                
 
             '''Clears the terminal'''
-            os.system('clear') # clears the terminal
+            os.system('clear')
 
-
-        def do_echo(self,arg):
-                
+        def do_echo(self, arg):
 
             '''Prints the arguments to the terminal'''
             args = parse(arg)
@@ -151,7 +167,7 @@ class SeaTurtle(Cmd):
                     echoed = get_echo(comment)
                     try:
                         # outputs the string to the given file
-                        overwrite([echoed],args[i+1:])
+                        overwrite([echoed], args[i+1:])
                         break
                     except IndexError:
                         # prints this error message if no filename is given
@@ -164,7 +180,7 @@ class SeaTurtle(Cmd):
                     echoed = get_echo(comment)
                     try:
                         # outputs the string to the given file
-                        append([echoed],args[i+1:])
+                        append([echoed], args[i+1:])
                         break
                     except IndexError:
                         # shows this error if no filename is given
@@ -175,38 +191,47 @@ class SeaTurtle(Cmd):
                     # appends arguments that are not output commands to a list
                     comment.append(args[i])
                     count += 1
-            # If the loop did not break early, meaning there were no output commands
+            # If the loop did not break early
             if count == len(args):
                 # Print the concatenated list of arguments as a string
                 print(get_echo(comment))
 
+        def do_cd(self, arg):
 
-        def do_cd(self,arg):
-
-            '''changes directory do given directory or display current directory if no directroy is given'''
+            '''changes directory do given directory or
+            display current directory if no directroy is given'''
             args = parse(arg)
             try:
                 # changes directory to given directory
                 os.chdir(args[0])
                 os.environ['PWD'] = os.getcwd()
-                if os.environ['HOME'] == os.getcwd()[0:len(os.environ['HOME'])]:        
+                curr_dir = os.getcwd()[0:len(os.environ['HOME'])]
+                if os.environ['HOME'] == curr_dir:
                     # If CWD is in $HOME(/home/user/) display $HOME as "~/"
-                    SeaTurtle.prompt = '{}@{} ~{} $ '.format(os.environ['USER'],os.uname()[1],os.getcwd()[len(os.environ['HOME']):])
+                    ndir = os.getcwd()[len(os.environ['HOME']):]
+                    user = os.environ['USER']
+                    host = os.uname()[1]
+                    SeaTurtle.prompt = '{}@{} ~{} $ '.format(user, host, ndir)
                 else:
                     # otherwise display true directory
-                    SeaTurtle.prompt = '{}@{} {} $ '.format(os.environ['USER'],os.uname()[1],os.getcwd())
+                    ndir = os.getcwd()
+                    user = os.environ['USER']
+                    host = os.uname()[1]
+                    SeaTurtle.prompt = '{}@{} {} $ '.format(user, host, ndir)
             except FileNotFoundError:
                 # displays this error message if the given file does not exist
                 print('Error: No such directory')
             except IndexError:
                 print(os.getcwd())
                 # if no directory is given, change directory to $HOME
-                #os.chdir(os.environ['HOME'])
-                #SeaTurtle.prompt = '{}@{} ~{} $ '.format(os.environ['USER'],os.uname()[1],os.getcwd()[len(os.environ['HOME']):])
-
+                # os.chdir(os.environ['HOME'])
+                # home = os.getcwd()[len(os.environ['HOME']):]
+                # user = os.environ['USER']
+                # host = os.uname()[1]
+                # SeaTurtle.prompt = '{}@{} ~{} $ '.format(user, host, home)
 
         def do_environ(self, arg):
-                
+
             '''prints all environment variables, separated by \n'''
             # converts arg to a list
             args = parse(arg)
@@ -222,8 +247,8 @@ class SeaTurtle(Cmd):
                         print('Usage: environ > <filename>')
                 # If using append
                 elif args[0] == '>>':
-                    try:    
-                        # Appends output to the given file 
+                    try:
+                        # Appends output to the given file
                         append(get_environ(), args[1:])
                     except IndexError:
                         # Print this Error message if no filename is given
@@ -232,8 +257,7 @@ class SeaTurtle(Cmd):
             except IndexError:
                 print("\n".join(get_environ()))
 
-
-        def do_quit(self,arg):
+        def do_quit(self, arg):
 
             '''Exits the shell'''
             exit()
@@ -241,16 +265,15 @@ class SeaTurtle(Cmd):
 
 def get_environ():
 
-
-    '''returns a list containing all the environment variables and their values as strings'''
+    '''returns a list containing all the environment
+    variables and their values as strings'''
     env_list = []
     for k in os.environ:
-        env_list.append('{} : {}'.format(k, os.environ[k]))     
+        env_list.append('{} : {}'.format(k, os.environ[k]))
     return env_list
 
 
 def get_echo(comment):
-
 
     '''Concatenates a list to a single string'''
     return " ".join(comment)
@@ -258,13 +281,12 @@ def get_echo(comment):
 
 def ls_dir(directory=None):
 
-
     '''Returns the contents of a directory as a string'''
     try:
         # If a directory is specified
-        if  directory != None:
-            # Return a string containing all the contents of the specified directory
-            return  "\n".join([f for f in os.listdir(directory)])
+        if directory is not None:
+            # Return a string containing all the contents
+            return "\n".join([f for f in os.listdir(directory)])
         # If no directory is specified
         else:
             # Return a string containing the contents of the current directory
@@ -272,45 +294,46 @@ def ls_dir(directory=None):
     except FileNotFoundError:
         # Shows this error message if the directory does not exist
         print('Error: Directory "{}" not found'.format(directory))
-                
 
 
 def from_input(filename):
-        
 
     '''gets content from the specified file and returns it as a list'''
     try:
-        with open(filename,'r') as f:
-            return [args.strip() for args in f.readlines()] # returns a list of all the lines contained in the file
+        with open(filename, 'r') as f:
+            # returns a list of all the lines contained in the file
+            return [args.strip() for args in f.readlines()]
     except FileNotFoundError:
-        print('Error: File "{}" not found'.format(filename))  # Shows this error message if the file does not exist
+        # Shows this error message if the file does not exist
+        print('Error: File "{}" not found'.format(filename))
 
 
-def overwrite(data,args):
+def overwrite(data, args):
 
-
-    '''overwrites any data within a file with new data, or creates a new file containing the new data if one doesn't exist'''
+    '''overwrites any data within a file with new data,
+    or creates a new file containing the new data if one doesn't exist'''
     try:
         # Opens the file, or creates one with that name if it doesn't exist
         with open(args[0], 'w+') as f:
             for a in data:
-                # Writes the data to the file, overwriting any data that was already contained by the file 
+                # Writes the data to the file
                 f.write(a)
                 # Adds a newline character to the end of the file
                 f.write('\n')
-        except IndexError:
-                # Shows this error message if no file was specified
-                print('Usage: <command> > <filename>')
+    except IndexError:
+        # Shows this error message if no file was specified
+        print('Usage: <command> > <filename>')
 
 
-def append(data,args):
+def append(data, args):
 
-    '''appends new data to a file, or creates a file containing the new data if one does not exist'''
+    '''appends new data to a file, or creates a file containing the new data if
+    one does not exist'''
     try:
         # Opens the file, or creates one with that name if it doesn't exist
         with open(args[0], 'a+') as f:
             for a in data:
-                # Writes the data to the file, appending to the end of the file if there was already data within the file
+                # Writes the data to the file
                 f.write(a)
                 # Adds a newline charater to the end of the file
                 f.write('\n')
@@ -318,10 +341,11 @@ def append(data,args):
         # Shows this error message if no filename was specified
         print('Usage: <command> >> <filename>')
 
-        
+
 def parse(arg):
-        
-    '''Gets the command line arguments and returns them as list without the orinal command'''
+
+    '''Gets the command line arguments and returns them as list without the
+    original command'''
     return arg.split()
 
 
